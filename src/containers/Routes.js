5b8c9475fbce0f6beb1b4a090e7a1badb00bec15
Alex <darjeilolol@gmail.com>
2020-01-20 16:43:44 +0100

    Setting up our React app

--- src/containers/Routes.js
+++ src/containers/Routes.js
@@ -4,7 +4,7 @@ import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 
 export default () =>
-  <Switch>
-    <Route path="/" exact component={Home} />
-    <Route component={NotFound} />
-  </Switch>;
+    <Switch>
+        <Route path="/" exact component={Home} />
+        <Route component={NotFound} />
+    </Switch>;
