7a6380ba0697c9204367702829adeea1e86bb430
Alex <darjeilolol@gmail.com>
2020-01-21 03:46:54 +0100

    final development

--- src/config.js
+++ src/config.js
@@ -1,16 +1,17 @@
 export default {
-        s3: {
-            REGION: "eu-west-3",
-            BUCKET: "notes-app-tuto"
-        },
-        apiGateway: {
-            REGION: "us-east-2",
-            URL: "https://jvb4iau51b.execute-api.us-east-2.amazonaws.com/prod"
-        },
-        cognito: {
-            REGION: "us-east-2",
-            USER_POOL_ID: "us-east-2_iJjBL5T0K",
-            APP_CLIENT_ID: "165pi7ckaqjpgaauimvsgtjn3i",
-            IDENTITY_POOL_ID: "us-east-2:db3f752f-50e5-4bb4-aaf1-a3cedaa5e2b6"
-        }
+    MAX_ATTACHMENT_SIZE: 5000000,
+    s3: {
+        REGION: "eu-west-3",
+        BUCKET: "notes-app-tuto"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://jvb4iau51b.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_iJjBL5T0K",
+        APP_CLIENT_ID: "165pi7ckaqjpgaauimvsgtjn3i",
+        IDENTITY_POOL_ID: "us-east-2:db3f752f-50e5-4bb4-aaf1-a3cedaa5e2b6"
+    }
 };
